d4ede1cf854d3b664efe7b2bdba04da912ca4351
griff1 <gpmaster117@gmail.com>
2014-11-10 15:55:07 -0800

    Removed unused clear button and updated tokens
    Looking back at project for first time in a while.  Updated tokens and
    tweaked card display.  Not definition does not seem to work anymore.
    Main.js is not even running, so something is crashing before there.
    This was happening before I made any changes, so I’m not sure what’s
    happening.  Will look into it.

--- js/chooseFile.js
+++ js/chooseFile.js
@@ -1,7 +1,7 @@
 // Temporary until we get a real note store url.
 var NOTESTORE_URL = "https://sandbox.evernote.com/shard/s1/notestore";
 // Temporary auth token
-var AUTH_TOKEN = "S=s1:U=8f620:E=14fa5daa7aa:C=1484e297970:P=1cd:A=en-devtoken:V=2:H=80e76d6bd18b52024160457e7add54b1";
+var AUTH_TOKEN = "S=s1:U=8f620:E=150f3d2090c:C=1499c20d9d8:P=1cd:A=en-devtoken:V=2:H=89178b440448e101c0fa92c3a611b872";
 
 var noteStoreTransport = new Thrift.BinaryHttpTransport(NOTESTORE_URL);
 var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
--- js/content.js
+++ js/content.js
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 console.log("HELLO")
 var noteStoreURL = "https://sandbox.evernote.com/shard/s1/notestore";
-var authenticationToken = "S=s1:U=8f620:E=14fa4017722:C=1484c5048b8:P=1cd:A=en-devtoken:V=2:H=dc59daf40914508324c07af0ac591d34";
+var authenticationToken = "S=s1:U=8f620:E=150f3d2090c:C=1499c20d9d8:P=1cd:A=en-devtoken:V=2:H=89178b440448e101c0fa92c3a611b872";
 var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
 var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
 var noteStore = new NoteStoreClient(noteStoreProtocol);
